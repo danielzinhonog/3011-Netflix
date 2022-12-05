cc99a71df2bd433b492c5b6e9c76edc9a2a812d0
Daniel Nogueira <113999917+danielzinhonog@users.noreply.github.com>
2022-12-05 10:53:23 -0300

    Retoques
    Acertando algumas coisas por pura estética.

--- cadastro.php
+++ cadastro.php
@@ -61,8 +61,7 @@ p{
 </style>
 
 <body class="body">
-<img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
->
+<img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png">
 <div class="form">
 <h1>Entrar</h1>
     <form method="POST">
