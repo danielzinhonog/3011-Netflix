46c8204c0a9965ee345f9eaec07ac780bd02ab4d
Daniel Nogueira <113999917+danielzinhonog@users.noreply.github.com>
2022-12-05 10:55:00 -0300

    Retoques
    Apenas umas melhorias estéticas.

--- config.php
+++ config.php
@@ -1,13 +1,8 @@
 <?php
-
 $conexao = new mysqli('localhost:3306', 'root', '', 'netflix');
-
-if($conexao->connect_errno)
-{
+if($conexao->connect_errno){
     echo "Tem alguma coisa errada";
-}
-else{
+}else{
     echo "Conexão bem sucedida";
 }
-
 ?>
