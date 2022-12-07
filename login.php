ef2a8b5a3d378d92700e627bc9b356df8beafbc6
Daniel Nogueira <113999917+danielzinhonog@users.noreply.github.com>
2022-12-07 11:52:33 -0300

    Logo
    Adicionei a logo do site.

--- login.php
+++ login.php
@@ -13,6 +13,7 @@ if(isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://i.pinimg.com/originals/0f/66/dc/0f66dc3fe8ea994a70ed78472b2aa50f.png" rel="icon"> 
     <title>Login</title>
 </head>
 
